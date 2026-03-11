713ea1648419285b9cca39d1be912dcb7ff5bab1
Robert de S. Maio <robertmaio@outlook.com>
2026-03-11 20:20:45 -0300

    add notification preview

--- src/pages/AdminPainel.tsx
+++ src/pages/AdminPainel.tsx
@@ -1,6 +1,6 @@
 import { Form, Formik, useFormikContext } from "formik";
 import { useEffect, useState } from "react";
-import Classes from "../Features/components/Classes";
+import NotificationCard from "../Features/components/NotificationCard";
 import NotificationForm from "../Features/components/NotificationForms";
 
 interface NotificationValues {
@@ -40,7 +40,7 @@ export default function AdminPainel() {
     <main className="min-h-full flex-1">
       <div className="grid grid-cols-2 md:grid-cols-2 gap-3 p-6">
         <div className="max-w-2xl mx-auto p-6 bg-white rounded-xl shadow-md border border-slate-200">
-          <Classes />
+          <NotificationCard notification={previewValues} />
         </div>
         <div className="p-10">
           <Formik
