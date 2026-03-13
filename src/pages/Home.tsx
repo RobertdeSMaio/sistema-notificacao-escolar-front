7e1a7a26fbc30d86fe69fab6a469b747b5940edf
Robert de S. Maio <robertmaio@outlook.com>
2026-03-13 09:07:45 -0300

    Api get consuming

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,35 @@
-import Cards from "../components/layout/Cards";
+import { useEffect, useState } from "react";
+import NotificationCard from "../Features/components/NotificationCard";
 
 export default function Home() {
+  const [avisos, setAvisos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(
+      "https://sistema-notificacao-escolar-back.onrender.com/api/Notification",
+      {
+        method: "GET",
+      },
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setAvisos(data);
+        setLoading(false);
+      })
+      .catch((err) => console.error("Erro ao buscar avisos", err));
+  }, []);
+
+  if (loading) return <p className="p-6">Carregando avisos . . . </p>;
   return (
-    <>
-      <div className="flex min-h-screen bg-slate-100">
-        <Cards />
-      </div>
-    </>
+    <div className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {avisos.length > 0 ? (
+        avisos.map((item) => (
+          <NotificationCard key={item.id} notification={item} />
+        ))
+      ) : (
+        <p>Nenhum comunicado encontrado.</p>
+      )}
+    </div>
   );
 }
