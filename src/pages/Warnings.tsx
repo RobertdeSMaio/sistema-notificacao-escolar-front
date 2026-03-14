826c1e0813e5fdd8abb79d75c8bca6036e9b1ef6
Robert de S. Maio <robertmaio@outlook.com>
2026-03-14 12:59:57 -0300

    grid

--- src/pages/Warnings.tsx
+++ src/pages/Warnings.tsx
@@ -41,7 +41,7 @@ export default function NotificationDetail() {
           <NotificationCard key={item.id} notification={item} />
         ))
       ) : (
-        <p className="p-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <p className="p-10 text-center text-gray-600">
           Nenhum comunicado encontrado.
         </p>
       )}
